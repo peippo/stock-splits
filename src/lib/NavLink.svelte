e854cbed348531887798535c0f7ddbb90f9f4426
Janne Peippo <janne.peippo@poutapilvi.fi>
2022-02-26 20:02:11 +0200

    Add nav styling

---
+++ src/lib/NavLink.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { page } from '$app/stores';
+
+	export let title;
+	export let href;
+	export let type;
+</script>
+
+<a
+	{href}
+	class={type === 'year' ? 'nav__link nav__link--year' : 'nav__link'}
+	class:active={$page.url.pathname.includes(href)}
+>
+	{title}
+</a>
+
+<style lang="scss">
+	.nav__link {
+		text-decoration: none;
+		color: var(--header-link-color);
+		padding: 0.15rem 0.25rem;
+		border-radius: 3px;
+
+		&:not(.nav__link--year) {
+			@media (max-width: 800px) {
+				display: none;
+			}
+		}
+	}
+
+	.nav__link--year {
+		font-weight: 700;
+	}
+
+	.active {
+		background-color: var(--header-link-color);
+		color: #fff;
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,49 @@
+<script>
+	import NavLink from '$lib/NavLink.svelte';
+	import '../app.scss';
+</script>
+
+<header>
+	<h1><a href="/">Stock/Splits</a></h1>
+	<nav>
+		<NavLink href="/year/2020" title="2020" type="year" />
+		<NavLink href="/year/2020/quarter/q3" title="Q3" />
+		<NavLink href="/year/2020/quarter/q4" title="Q4" />
+		<NavLink href="/year/2021" title="2021" type="year" />
+		<NavLink href="/year/2021/quarter/q1" title="Q1" />
+		<NavLink href="/year/2021/quarter/q2" title="Q2" />
+		<NavLink href="/year/2021/quarter/q3" title="Q3" />
+		<NavLink href="/year/2021/quarter/q4" title="Q4" />
+		<NavLink href="/year/2022" title="2022" type="year" />
+		<NavLink href="/year/2022/quarter/q1" title="Q1" />
+	</nav>
+</header>
+
+<slot />
+
+<style lang="scss">
+	header {
+		position: relative;
+		background-color: var(--header-background-color);
+		display: flex;
+		align-items: center;
+		padding: 0.5rem 1rem;
+		box-shadow: 0 5px 5px -2px #272f3a29;
+		z-index: 1;
+	}
+
+	h1 {
+		margin: 0;
+		text-transform: uppercase;
+		font-size: 1.25rem;
+
+		a {
+			text-decoration: none;
+			color: #fff;
+		}
+	}
+
+	nav {
+		margin-left: 1rem;
+	}
+</style>
