15b4851113692d66c78fbf0b9f7be0051c80d1e8
Janne Peippo <janne.peippo@poutapilvi.fi>
2022-02-28 17:30:43 +0200

    Adjust mobile logo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,13 @@
 </script>
 
 <header>
-	<h1><a href="/">Stock/Splits</a></h1>
+	<h1>
+		<a href="/">
+			S<span class="hidden-mobile">tock</span><span class="divider">/</span>S<span
+				class="hidden-mobile">plits</span
+			>
+		</a>
+	</h1>
 	<nav aria-label="Main">
 		<NavLink href="/year/2020" title="2020" type="year" />
 		<NavLink href="/year/2020/quarter/q3" title="Q3" type="quarter" />
@@ -41,6 +47,16 @@
 		a {
 			text-decoration: none;
 			color: #fff;
+		}
+	}
+
+	.divider {
+		opacity: 0.75;
+	}
+
+	@media (max-width: 500px) {
+		.hidden-mobile {
+			display: none;
 		}
 	}
 
